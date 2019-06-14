afbdc3ba59e96661da65b1d53b32735b6e3cb8ac
VVMaestro <38831071+VVMaestro@users.noreply.github.com>
2019-06-14 12:15:07 +0300

    ÐРеализует функцию добавления гномом новых камней

--- add-gems.php
+++ add-gems.php
@@ -10,6 +10,16 @@ $user = $_SESSION['user'];
 $title = 'Добавление камней';
 
 if ($user && ($user['role'] == 'dwarf' || 'master-dwarf')) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        foreach ($_POST as $gem_type => $gem_amount) {
+            for ($i = 0; $i < $gem_amount; $i++) {
+                $sql_request = 'INSERT INTO gems (TYPE, mine_dwarf, mine_date, STATUS)
+                VALUES (' . $gem_type . ', ' . $user['id'] . ', CURDATE(), "not_assigned")';
+                change_db_data($connect, $sql_request);
+            }
+        }
+    }
+
     $gem_types = get_db_data($connect, $gem_types_request);
 
     $page_content = renderTemplate('add-gems.tmp', [
--- templates/add-gems.tmp.php
+++ templates/add-gems.tmp.php
@@ -3,16 +3,16 @@
         <section class="page__section">
             <h2 class="page__title">Добавить драгоценности</h2>
             <div class="add-gems__colums">
-                <ul class="add-gems__list">
-                    <form action="" method="POST" id="add-gems">
+                <form action="/add-gems.php" method="POST" id="add-gems">
+                    <ul class="add-gems__list">
                         <?php foreach ($gem_types as $type) : ?>
                             <li class="add-gems__item">
                                 <span class="add-gems__name"> <?= $type['name']; ?> </span>
-                                <input type="number" name="<?= $type['name']; ?>" class="input add-gems__input" value="0" min="0">
+                                <input type="number" name="<?= $type['id']; ?>" class="input add-gems__input" value="0" min="0">
                             </li>
                         <?php endforeach; ?>
-                    </form>
-                </ul>
+                    </ul>
+                </form>
                 <input type="submit" class="button button--big" form="add-gems" value="Добавить камни">
             </div>
         </section>
